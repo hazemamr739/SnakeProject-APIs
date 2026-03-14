931eba1c7ba559f81ca31c12247df9bd506b57ea
Hazemamr <hazemamr165@gmail.com>
2026-03-14 16:25:42 +0200

    update backend dependencies and refresh package versions update dependencies and refine backend architecture reorganize solution architecture and align package references

---
+++ SnakeProject.Domain/Entities/GameShareAccount.cs
@@ -0,0 +1,18 @@
+﻿using SnakeProject.Domain.Enums;
+
+namespace SnakeProject_BE.Entities
+{
+    public class GameShareAccount
+    {
+        public int Id { get; set; }
+
+        public AccessType AccessType { get; set; }   // Primary, Secondary, Full
+        public ConsoleType Console { get; set; }     // PS4, PS5, Both
+
+        public decimal Price { get; set; }
+
+        public bool IsActive { get; set; }
+
+        
+    }
+}
---
+++ SnakeProject.Domain/Enums/Enums.cs
@@ -0,0 +1,62 @@
+﻿namespace SnakeProject.Domain.Enums
+{
+    public enum ProductType : byte
+    {
+        Account = 1,
+        Subscription = 2,
+        PsnCode = 3
+    }
+
+    public enum AccessType : byte
+    {
+        FullAccount = 1,
+        Primary = 2,
+        Secondary = 3
+    }
+    public enum Currency
+    {
+        USD = 1,
+        EUR = 2,
+        GBP = 3
+    }
+    public enum PlusPlan : byte
+    {
+        essintial = 1,
+        extra = 2,
+        deluxe = 3
+    }
+    public enum SubscriptionDuration
+    {
+        OneMonth = 1,
+        ThreeMonths = 3,
+        TwelveMonths = 12
+    }
+    public enum ConsoleType
+    {
+        PS5 = 1,
+        PS4 = 2
+    }
+    public enum InventoryStatus
+    {
+        Available = 1,
+        Reserved = 2,
+        Sold = 3
+    }
+
+    public enum OrderStatus
+    {
+        Pending = 1,
+        Paid = 2,
+        Processing = 3,
+        Completed = 4,
+        Cancelled = 5,
+        Failed = 6
+    }
+    public enum CartStatus
+    {
+        Active = 1,
+        Converted = 2,
+        Abandoned = 3
+    }
+
+}
