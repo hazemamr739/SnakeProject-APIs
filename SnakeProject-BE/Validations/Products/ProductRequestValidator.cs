b31b2d9937a9b2675a470a22fdd11025dd76474a
Hazemamr <hazemamr165@gmail.com>
2026-03-14 13:25:05 +0200

    edit PsnCodeRequestValidator and adding enhancement

--- SnakeProject-BE/Validations/Products/ProductRequestValidator.cs
+++ SnakeProject-BE/Validations/Products/ProductRequestValidator.cs
@@ -2,21 +2,11 @@
 
 namespace SnakeProject_BE.Validations.Products
 {
-    public class ProductRequestValidator : AbstractValidator<PsnCodeRequest>
+    public class ProductRequestValidator : AbstractValidator<ProductRequest>
     {
         public ProductRequestValidator()
         {
-            RuleFor(p => p.Code)
-                .NotEmpty().WithMessage("Code is required.")
-                .Length(4, 30).WithMessage("Code must be between 4 and 30 characters.");
-
-             RuleFor(p => p.ProductId)
-                .GreaterThan(0).WithMessage("ProductId must be a positive number.");
-
-
-            RuleFor(x => x.UsedAt)
-                .Equal(default(DateTime))
-                .WithMessage("UsedAt must be empty when IsUsed is false.");
+           
         }
     }
 }
--- SnakeProject-BE/Validations/Products/PsnCodeRequestValidator.cs
+++ SnakeProject-BE/Validations/Products/PsnCodeRequestValidator.cs
@@ -6,7 +6,17 @@ namespace SnakeProject_BE.Validations.Products
     {
         public PsnCodeRequestValidator()
         {
-            RuleFor(p=>p.)
+            RuleFor(p => p.Code)
+                .NotEmpty().WithMessage("Code is required.")
+                .Length(4, 30).WithMessage("Code must be between 4 and 30 characters.");
+
+            RuleFor(p => p.ProductId)
+               .GreaterThan(0).WithMessage("ProductId must be a positive number.");
+
+
+            RuleFor(x => x.UsedAt)
+                .Equal(default(DateTime))
+                .WithMessage("UsedAt must be empty when IsUsed is false.");
         }
     }
 }
