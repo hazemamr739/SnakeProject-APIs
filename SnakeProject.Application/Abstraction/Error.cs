beda1f791c705b5c353f72ecb8678c5f8ecdccf9
Hazemamr <hazemamr165@gmail.com>
2026-03-15 13:29:22 +0200

    Implement Result Pattern but finished

---
+++ SnakeProject.Application/Abstraction/Error.cs
@@ -0,0 +1,7 @@
+﻿namespace SnakeProject.Application.Abstraction
+{
+    public record Error(string Code, string Description, int? StatusCode)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty, null);
+    }
+}
---
+++ SnakeProject.Application/ErrorHandling/GlobalExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SnakeProject.Application.ErrorHandling
+{
+    public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+    {
+        private readonly ILogger _logger = logger;
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError("Something went wrong :{Message}", exception.Message);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Internal Server Error",
+                Detail = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1"
+            };
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+            return true;
+        }
+    }
+}
