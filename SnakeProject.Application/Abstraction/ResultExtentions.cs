7155d4f994f41616168393c630ba0555e3c49c46
Hazemamr <hazemamr165@gmail.com>
2026-03-15 13:30:20 +0200

    finished result pattern

--- SnakeProject.Application/Abstraction/ResultExtentions.cs
+++ SnakeProject.Application/Abstraction/ResultExtentions.cs
@@ -27,3 +27,4 @@ namespace SnakeProject.Application.Abstraction
             return new ObjectResult(problemDetails);
         }
     }
+}
--- SnakeProject.Application/ErrorHandling/UserErrors.cs
+++ SnakeProject.Application/ErrorHandling/UserErrors.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using SnakeProject.Application.Abstraction;
 
 namespace SnakeProject.Application.ErrorHandling
 {
