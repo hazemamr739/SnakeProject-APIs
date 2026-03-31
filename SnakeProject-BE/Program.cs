3337d9396647660080c74b4236f79b95b007c4c4
Hazemamr <hazemamr165@gmail.com>
2026-03-31 17:47:42 +0200

    Add CORS policy to allow any origin, method, and header
    Added a global "AllowAll" CORS policy to the API, enabling requests from any origin, with any HTTP method and header. This facilitates cross-origin access for clients, which is useful for development and public APIs. Use with caution in production environments.

--- SnakeProject-BE/Program.cs
+++ SnakeProject-BE/Program.cs
@@ -10,6 +10,19 @@ namespace SnakeProject.API
             // Add services to the container.
             builder.Services.AddDependencies(builder.Configuration);
 
+            // Add CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    policy =>
+                    {
+                        policy
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -32,6 +45,9 @@ namespace SnakeProject.API
             }
 
             app.UseHttpsRedirection();
+
+            // Use CORS
+            app.UseCors("AllowAll");
 
             app.UseAuthentication();
             app.UseAuthorization();
